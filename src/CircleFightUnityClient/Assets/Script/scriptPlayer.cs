763f02a3bf211724047d01b8180fe38d3a2b2761
bilope <bilope@naver.com>
2014-02-13 18:55:01 -0800

    #15

--- src/CircleFightUnityClient/Assets/Script/scriptPlayer.cs
+++ src/CircleFightUnityClient/Assets/Script/scriptPlayer.cs
@@ -4,32 +4,42 @@ using System.Collections;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Object;
 
 public class PlayerInfo
 {
 	private float mX;
 	private float mY;
-	private float mVelocity;
+	private float mVelocityX;
+	private float mVelocityY;
 	private float mAcceleration;
 	private float mAngle;
 
 	public PlayerInfo()
 	{
-		mX = mY = mVelocity = mAcceleration = mAngle = 0;
+		mX = mY = mVelocityX = mVelocityY = mAcceleration = mAngle = 0;
 	}
-	public PlayerInfo(float pX, float pY, float pVelocity, float pAcceleration, float pAngle)
+	public PlayerInfo(float pX, float pY, float pVelocityX, float pVelocityY, float pAcceleration, float pAngle)
 	{
-		Init (pX, pY, pVelocity, pAcceleration, pAngle);
+		Init (pX, pY, pVelocityX, pVelocityY, pAcceleration, pAngle);
 	}
 
-	public void Init(float pX, float pY, float pVelocity, float pAcceleration, float pAngle)
+	public void Init(float pX, float pY, float pVelocityX, float pVelocityY, float pAcceleration, float pAngle)
 	{
 		mX = pX;
 		mY = pY;
-		mVelocity = pVelocity;
+		mVelocityX = pVelocityX;
+		mVelocityY = pVelocityY;
 		mAcceleration = pAcceleration;
 		mAngle = pAngle;
+	}
+
+	public void Update()
+	{
+		mX = mX + mVelocityX * Time.deltaTime;
+		mY = mY + mVelocityY * Time.deltaTime;
+
+		mVelocityX = mVelocityX + mAcceleration * Time.deltaTime;
+		mVelocityY = mVelocityY + mAcceleration * Time.deltaTime;
 	}
 
 	public void SetPosition (float pX, float pY)
@@ -37,9 +47,18 @@ public class PlayerInfo
 		mX = pX;
 		mY = pY;
 	}
-	public void SetVelocity(float pVelocity)
+	public void SetVelocity(float pVelocityX, float pVelocityY)
 	{
-		mVelocity = pVelocity;
+		mVelocityX = pVelocityX;
+		mVelocityY = pVelocityY;
+	}
+	public void SetVelocityX(float pVelocityX)
+	{
+		mVelocityX = pVelocityX;
+	}
+	public void SetVelocityY(float pVelocityY)
+	{
+		mVelocityY = pVelocityY;
 	}
 	public void SetAcceleration(float pAcceleration)
 	{
@@ -77,9 +96,7 @@ public class scriptPlayer : MonoBehaviour {
 		client.Init ();
 		speed = 1.0f;
 
-		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
-		thePerfab = spriteRenderer.sprite;
-		instance = (Sprite)Instantiate (thePerfab, transform.position, transform.rotation);
+
 	}
 	
 	// Update is called once per frame
