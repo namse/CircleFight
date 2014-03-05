115deb867b7992bc19f7a083fa03c386e72ce63d
bilope <bilope@naver.com>
2014-03-05 00:25:49 -0800

    로그인 ui 등 추가

--- src/CircleFightUnityClient/Assets/Script/GameManager.cs
+++ src/CircleFightUnityClient/Assets/Script/GameManager.cs
@@ -13,8 +13,60 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		// 로그인 UI
+		if (GameStatus == 0)
+		{
+			string_id = GUI.TextField (new Rect ((Screen.width - 200) / 2, Screen.height / 2 + 30, 200, 20), string_id, 20);
+			string_pw = GUI.TextField (new Rect ((Screen.width - 200) / 2, Screen.height / 2 + 60, 200, 20), string_pw, 20);
+			
+			if (GUI.Button (new Rect ((Screen.width - 200) / 2, Screen.height / 2 + 90, 200, 20), "Login"))
+			{
+				GameStatus = 1;
+				
+				Debug.Log ("Login Clicked!");
+			}
+		}
+		// 서버로 부터 응답을 기다린다.
+		else if (GameStatus == 1)
+		{
+			GameStatus = 2;
+			
+			// 응답이 오고, 필요한 패킷들을 모두 받은 후 로그인 UI를 삭제하고 리스폰 UI를 띄운다.
+			Destroy(GameObject.Find ("CircleFightLoginUI"));
+			Instantiate(prefab_CircleFightResponeMenu);
+		}
+		// 자신의 마지막 위치를 중심으로 시뮬레이션해주고, 리스폰 메뉴를 띄운다.
+		else if (GameStatus == 2)
+		{
+			if (Input.GetMouseButtonDown (0))
+			{
+				if (59 <= Input.mousePosition.y && Input.mousePosition.y <= 245)
+				{
+					// Red 
+					if(68 <= Input.mousePosition.x && Input.mousePosition.x <= 254)
+					{
+						Debug.Log("Red");
+					}
+					// Blue
+					if(273 <= Input.mousePosition.x && Input.mousePosition.x <= 459)
+					{
+						Debug.Log("Blue");
+					}
+					// Green
+					if(479 <= Input.mousePosition.x && Input.mousePosition.x <= 665)
+					{
+						Debug.Log("Green");
+					}
+				}
+			}
+		}
+		// 게임 플레이 중 ...
+		else if (GameStatus == 3)
+		{
+			
+		}
 	}
 
 	// GUI
